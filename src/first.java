74fbe0cb103c47fe06ec77990fdea4127f3fbef6
a <aselaiisha25@gmail.com>
2019-11-09 14:56:47 -0600

    set up

--- src/first.java
+++ src/first.java
@@ -1,5 +1,8 @@
 public class first {
     public static void main(String[] args) {
         System.out.println("hello");
+        if(true) {
+            System.out.println("bye");
+        }
     }
 }
